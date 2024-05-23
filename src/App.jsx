f2860cfdc561b6300fbe4cc8d37e261993d7a505
SupRMinecraftien <superminecraftien.levrai@gmail.com>
2024-05-23 19:19:23 +0200

    Created header, added the chess.com background

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import chessLogo from '/chessLong.png'
 import './App.css'
+import { useEffect } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function Main() {
+
+  useEffect(() => {
+    setInterval(clockUpdate, 1000)
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <img src={chessLogo} alt='Chess.com' />
+        <h1>Tournament</h1>
+        <div id='clockTime'></div>
+      </header>
     </>
+    
   )
 }
 
-export default App
+function clockUpdate() {
+  const date = new Date()
+  const time = date.toLocaleTimeString()
+  document.getElementById('clockTime').textContent = time
+}
